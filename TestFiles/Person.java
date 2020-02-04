a2427fee74a9d6f14e25f117b5649955966e8470
John Paukovits <sirlaxalot24@yahoo.com>
2020-02-03 19:36:19 -0500

    added Chapt 3 and worked on HW

---
+++ TestFiles/Person.java
@@ -0,0 +1,18 @@
+
+
+public class Person {
+    private String name;
+    
+    // method to set the name in the employee object
+    public void setName(String name) {
+        this.name = name; //store the name
+    }
+
+    // method to return string name from the employee object
+    public String getName() {
+        return name; // return the value of the name
+    }
+
+    Person name = new setName("John");
+
+};
---
+++ TestFiles/PersonTest.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class PersonTest {
+    public static void main(String[] args) {
+
+        Person name = new setName("John");
+
+    }
+}
