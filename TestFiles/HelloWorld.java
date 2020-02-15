0ddffd10f87ccd461a35ce7d8083336886548417
sirlaxalot24 <j.a.paukovits@gmail.com>
2020-02-15 12:00:39 -0500

    Finished hw 4

--- TestFiles/HelloWorld.java
+++ TestFiles/HelloWorld.java
@@ -6,6 +6,11 @@ class HelloWorld
 	// Prints "Hello, World" to the terminal window. 
 	public static void main(String args[]) 
 	{ 
-		System.out.println("Hello, World"); 
+		int a = 5;
+		int b = 10 * a++;
+		int c = b * ++a;
+
+		System.out.printf("c = %d%n", c);
 	} 
-} 
+}
+
--- TestFiles/PersonTest.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class PersonTest {
-    public static void main(String[] args) {
-
-        Person name = new setName("John");
-
-    }
-}
