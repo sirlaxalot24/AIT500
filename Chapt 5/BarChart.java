d5db5486ab5dd818c2b308a8553d816d563d5c91
sirlaxalot24 <j.a.paukovits@gmail.com>
2020-02-20 18:50:14 -0500

    Added Chapt 5

---
+++ Chapt 5/BarChart.java
@@ -0,0 +1,16 @@
+public class BarChart {
+    private int numberOfStars;
+
+    public BarChart(int numberOfStars) {
+        this.numberOfStars = numberOfStars;
+    }
+
+    public void displayBar() {
+
+        // Create a loop to execute the following statement this.numberOfStars times
+        System.out.print("*");
+
+        // After the loop
+        System.out.println();
+    }
+}
---
+++ Chapt 5/BarChartTester.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BarChartTester {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Loop five times, and within the loop do the following:
+        // Prompt for a number
+        // create a BarChart object
+        // call your object's displayBar method
+    }
+
+}
