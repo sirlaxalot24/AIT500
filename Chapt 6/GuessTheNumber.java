16fcde3b603296340de59ee1d2ecdac9e9f6ca36
John Paukovits <sirlaxalot24@yahoo.com>
2020-02-28 16:12:47 -0500

    Finished programming assignment 6

--- Chapt 6/GuessTheNumber.java
+++ Chapt 6/GuessTheNumber.java
@@ -9,24 +9,45 @@ public class GuessTheNumber {
     public void play() {
     	int magicNumber = 1 + randomNumbers.nextInt(10);
     	numberOfGuess = 0;
-    	int guess = askForGuess();
-    	
-    	// Check if the guess is equal to the magicNumber
+		int guess = askForGuess();
+		// Check if the guess is equal to the magicNumber
     	// if it is, display the guess is correct and tell how many guesses
     	// otherwise, tell the user if the guessed too high or too low
-    	// and ask for a new guess
+		// and ask for a new guess
+		
+		while (guess != magicNumber){
+			if(guess < magicNumber){
+				System.out.println("You guess was low");
+				guess = askForGuess();
+			} else {
+				System.out.println("Your guess was high");
+				guess = askForGuess();
+			}
+		}
+
+		System.out.println(guess + " Is correct!!!!!!");
+		System.out.println("You guessed correct in " + numberOfGuess + " guesses");
     }
     
     private int askForGuess() {
     	int guess = 0;
-    	
     	// Prompt for user's guess
     	// Make sure the guess is between 1 and 10 inclusive
     	// If the guess is an error, display the error message and ask for new guess
     	
     	// If the guess is a valid guess (1 - 10 inclusive)
-    	// increment numberOfGuess
-    	return guess;
+		// increment numberOfGuess
+		
+		System.out.println("Please guess a number from 1 to 10");
+		guess = input.nextInt();
+
+		while ((guess < 1) || (guess > 10)){
+			System.out.println("You entered a number less than 1 or greater than 10");
+			System.out.println("Please enter a number from 1 to 10");
+			guess = input.nextInt();
+		}
+
+		numberOfGuess ++;
+		return guess;
     }
-	
 }
--- Chapt 6/GuessTheNumberTest.java
+++ Chapt 6/GuessTheNumberTest.java
@@ -1,14 +1,24 @@
+import java.lang.ProcessBuilder.Redirect.Type;
 import java.util.Scanner;
 
 public class GuessTheNumberTest {
 	
 	public static void main(String [] args) {
+		
 		Scanner input = new Scanner(System.in);
-		
 		GuessTheNumber game = new GuessTheNumber();
-		
 		game.play();
-		// ask to play again, maybe use a do...while loop
-	}
+		String keepPlaying = "Y";
 
+		do{
+			System.out.println("Enter Y to keep playing otherwise enter N");
+			keepPlaying = input.nextLine();
+			if(keepPlaying.equalsIgnoreCase("Y")){
+				System.out.println("*********New Game**********");
+				game.play();
+			} else {
+				System.out.println("!!!!!Thank you for playing, please come again!!!!!!");
+			}
+		} while (keepPlaying.equalsIgnoreCase("Y"));
+	}
 }
