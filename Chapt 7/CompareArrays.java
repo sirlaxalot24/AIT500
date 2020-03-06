6474abeaf34fb28096d835df5e80c92c68b693fe
John <sirlaxalot24@yahoo.com>
2020-03-06 13:47:47 -0500

    finished program 7

--- Chapt 7/CompareArrays.java
+++ Chapt 7/CompareArrays.java
@@ -5,6 +5,21 @@ public class CompareArrays {
         // Whatever is needed to see if arrayOne and arrayTwo are equal
         // You are not allowed to use Arrays.equals method
 
+        if ((arrayOne.length < 1) || (arrayTwo.length < 1)) {
+            areEqual = false;
+        } else if (arrayOne.length != arrayTwo.length) {
+            areEqual = false;
+        } else {
+            for (int i = 0; i < arrayOne.length; i++) {
+                if (arrayOne[i] != arrayTwo[i]){
+                    areEqual = false;
+                } else {
+                    areEqual = true;
+                }
+            }
+        }
+
+
         return areEqual;
     }
 }
--- Chapt 7/CompareArraysTest.java
+++ Chapt 7/CompareArraysTest.java
@@ -3,11 +3,28 @@ public class CompareArraysTest {
         // Compare equal arrays
         int [] a = {1, 2, 3, 4};
         int [] b = {1, 2, 3, 4};
-        System.out.println("Expect true " + CompareArrays.compare(a, b));
+        System.out.println("Expect true: " + CompareArrays.compare(a, b));
 
         // Describe what I'm about to test
-        int [] c = {};
-        int [] d = {};
-        System.out.println("Expect xxxxxx " + CompareArrays.compare(c, d));
+
+        // Test CompareArrays.compare to ensure order must match
+        int [] c = {1, 2, 3, 4};
+        int [] d = {4, 3, 2, 1};
+        System.out.println("Expect flase due to order: " + CompareArrays.compare(c, d));
+
+        // Test CompareArrays.compare to ensure lenght will fail
+        int [] e = {1, 2, 3, 4, 5};
+        int [] f = {4, 3, 2, 1};
+        System.out.println("Expect flase due to length: " + CompareArrays.compare(e, f));
+
+        // Test CompareArrays.compare to ensure full value will fail
+        int [] g = {1, 2, 3, 4};
+        int [] h = {10, 20, 30, 40};
+        System.out.println("Expect flase due to full value: " + CompareArrays.compare(g, h));
+
+        // Test CompareArrays.compare to ensure empty will fail
+        int [] i = {};
+        int [] j = {};
+        System.out.println("Expect flase due to empty: " + CompareArrays.compare(i, j));
     }
 }
