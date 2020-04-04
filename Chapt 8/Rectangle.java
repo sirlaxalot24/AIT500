282310fe2969fecd547daa912549d7d5edba5f81
John Paukovits <sirlaxalot24@yahoo.com>
2020-04-04 18:02:06 -0400

    half way through chapt 8

---
+++ Chapt 8/Rectangle.java
@@ -0,0 +1,39 @@
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height){
+        if(width <= 0 || width > 20 || height <= 0 || height > 20){
+            throw new IllegalArgumentException(
+                "Width or Heigh must be larger than zero and less than or equal to 20");
+        }
+        this.width = width;
+        this.height = height;
+}    
+
+    private void setParams(double width, double height){
+        if(width <= 0 || width > 20 || height <= 0 || height > 20){
+            throw new IllegalArgumentException(
+                "Width or Heigh must be larger than zero and less than or equal to 20");
+        }
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth(){
+        return width;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public double calcPerimeter(double width, double height){
+        return (width * 2) + (height *2);
+    }
+
+    public double calcArea(double width, double height){
+        return width *height;
+    }
+
+}
